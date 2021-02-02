fb1b9fc85f372c6018464ba8e441e25113f90d63
Nuno <nuno_c11@hotmail.com>
2021-02-02 10:53:42 +0000

    ft_split finally working.......

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,26 +6,25 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 16:59:38 by ngregori          #+#    #+#             */
-/*   Updated: 2021/02/01 19:52:51 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/02/02 10:35:24 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t		ft_count_words(char const *s, char c)
 {
 	size_t	word_count;
 	char	is_word;
 
-	word_count = -1;
+	word_count = 0;
 	is_word = 'n';
 	while (*s)
 	{
-		is_word == 'y' ? word_count+= 1 : word_count;
+		is_word == 'y' ? word_count += 1 : word_count;
 		while (*s != c && *s != '\0')
 			s++;
-		if (*s == c && *(s - 1) != c)
+		if (*s == c && *(s + 1) != c)
 			is_word = 'y';
 		else if (*s != '\0')
 			is_word = 'n';
@@ -33,33 +32,35 @@ size_t		ft_count_words(char const *s, char c)
 			return (word_count);
 		s++;
 	}
-	is_word == 'y' ? word_count +=1 : word_count;
+	is_word == 'y' ? word_count += 1 : word_count;
 	return (word_count);
 }
 
-char	**ft_split(char const *s, char c)
+char		**ft_split(char const *s, char c)
 {
-	char **arr;
-	char *sep_index;
-	char *sep_index2;
-	size_t words_nbr;
-	size_t i;
+	char	**arr;
+	size_t	sep_index;
+	size_t	next_index;
+	size_t	words_nbr;
+	size_t	i;
 
-	words_nbr = ft_count_words(s, c);
-	i = 0;
 	if (!s || !c)
 		return (NULL);
-	if(!(malloc(sizeof(char *) * (words_nbr + 1))))
+	i = -1;
+	sep_index = 0;
+	words_nbr = ft_count_words(s, c);
+	if (!(arr = malloc(sizeof(char *) * (words_nbr + 1))))
 		return (NULL);
-	while (i < words_nbr)
+	while (++i < words_nbr)
 	{
-		sep_index = ft_strchr(s, c);
-		sep_index2 = ft_strchr(sep_index, c);
-		arr[i] = strdup(sep_index, sep_index2)
+		while (s[sep_index] == c)
+			sep_index = ft_strchr(&s[sep_index], c) - s + 1;
+		next_index = ft_strchr(&s[sep_index], c)
+		? ft_strchr(&s[sep_index], c) - s - sep_index
+		: ft_strlen(s) - sep_index;
+		arr[i] = ft_substr(s, sep_index, next_index);
+		sep_index = ft_strchr(&s[sep_index], c) - s + 1;
 	}
-}
-
-int main(void)
-{
-	ft_split("i,i,,,,i,z,", ',');
+	arr[i] = 0;
+	return (arr);
 }
