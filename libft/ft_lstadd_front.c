4c531fde6a67360117e45e44155a70267bd811e2
Nuno <nuno_c11@hotmail.com>
2021-02-05 14:40:11 +0000

    ft_lastadd_front done

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 22:19:27 by ngregori          #+#    #+#             */
-/*   Updated: 2021/02/04 22:59:13 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/02/05 14:24:53 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (lst[0]->next == NULL)
-		lst[0]->next = new;
+	if (*lst == NULL)
+		*lst = new;
 	else
 	{
-		new->next = lst[0]->next;
-		lst[0]->next = new;
+		new->next = *lst;
+		(*lst) = new;
 	}
 }
