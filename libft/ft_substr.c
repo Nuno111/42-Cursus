8aa3e187841a695670eb97b4238861965f9b9c4d
Nuno <nuno_c11@hotmail.com>
2021-02-12 01:46:31 +0000

    small fixes

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 01:32:54 by ngregori          #+#    #+#             */
-/*   Updated: 2021/02/01 02:26:09 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/02/12 01:19:20 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub_str;
 
-	if (!(sub_str = malloc(sizeof(char) * len + 1)) || !len || !s)
+	if (!s)
+		return (NULL);
+	sub_str = malloc(sizeof(char) * len + 1);
+	if (!sub_str)
 		return (NULL);
 	if (start < ft_strlen(s))
 	{
