46b23a4c1f32802e6682b2b1181afc34797af9b3
Nuno <nuno_c11@hotmail.com>
2021-03-03 17:09:45 +0000

    s type almost there

--- printf/ft_printf/s_helpers.c
+++ printf/ft_printf/s_helpers.c
@@ -6,7 +6,7 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 15:37:36 by ngregori          #+#    #+#             */
-/*   Updated: 2021/03/03 17:00:11 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/03/03 17:09:26 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,23 @@ void	handle_s(t_node *n)
 {
 	char *tmp;
 	char *filler;
+	char *new;
 
 	tmp = va_arg(n->ap, char*);
+	if (n->prec_len < (long)ft_strlen(tmp) && n->has_prec)
+		new = truncate_str(tmp, n);
+	else
+		new = ft_strdup(tmp);
 	if (n->width_len > (long)ft_strlen(tmp))
 	{
 		filler = get_filler(tmp, n->width_len, n->pad_is_zero);
 		if (n->left_align)
-			n->buf = ft_strjoin(tmp, filler);
+			n->buf = str_join_free(&new, &filler);
 		else
-			n->buf = ft_strjoin(filler, tmp);
-		free(filler);
+			n->buf = str_join_free(&filler, &new);
 	}
 	else
-		n->buf = ft_strdup(tmp);
+		n->buf = new;
 	n->i++;
 	n->done = 1;
 }
