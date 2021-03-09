1f4a9cb6ff067b6120d154d1d6186b69a2584244
Nuno <nuno_c11@hotmail.com>
2021-03-09 21:59:08 +0000

    setting up env for basics

--- cub3d/main.c
+++ cub3d/main.c
@@ -1,8 +1,13 @@
 #include <mlx.h>
 
-int	main(void)
+int     main(void)
 {
-	void	*mlx;
+    void    *mlx;
+    void    *mlx_win;
 
-	mlx = mlx_init();
+    mlx = mlx_init();
+	if (!mlx)
+		return (-1);
+    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+    mlx_loop(mlx);
 }
