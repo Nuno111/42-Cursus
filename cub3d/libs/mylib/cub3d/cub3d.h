41c405248f78159f706e182c3bde4ff9741d4acc
Nuno <nuno_c11@hotmail.com>
2021-03-10 20:48:11 +0000

    adding a node to parse settings

--- cub3d/libs/mylib/cub3d/cub3d.h
+++ cub3d/libs/mylib/cub3d/cub3d.h
@@ -4,6 +4,24 @@
 #include "../libft/libft.h"
 #include "../ft_printf/ft_printf.h"
 
+typedef struct		s_res
+{
+	int		x;
+	int		y;
+}					t_res;
+
+typedef struct		s_scene
+{
+	t_res	res;
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	char	*sprite;
+	char	**floor;
+	char	**ceil;
+}					t_scene;
+
 bool	parse_name(char *map);
 bool	parse_settings(char *map);
 
--- cub3d/libs/mylib/cub3d/parse_settings.c
+++ cub3d/libs/mylib/cub3d/parse_settings.c
@@ -6,15 +6,41 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 18:20:30 by ngregori          #+#    #+#             */
-/*   Updated: 2021/03/10 18:21:49 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/03/10 20:41:29 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-bool    parse_settings(char *map)
+static	bool	handle_line(char *line)
 {
-	if (map)
-    	return (true);
-	return (false);
+	char *strs;
+
+	strs = ft_split(line, ' ');
+	if (!strs)
+		return (true);
+
+}
+
+bool    parse_settings(char *file)
+{
+	char *line;
+	int ret;
+	int fd;
+	bool status;
+	t_scene settings;
+
+	if (!file)
+		return (false);
+	status = true;
+	ret = 1;
+	fd = open(file, O_RDONLY);
+	while (ret > 0 && status)
+	{
+		line = NULL;
+		ret = get_next_line(fd, &line);
+		status = handle_line(line);
+		free(line);
+	}
+	return (status);
 }
