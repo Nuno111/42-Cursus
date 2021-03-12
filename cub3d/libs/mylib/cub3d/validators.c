99d1e3ab4574b826ac24571efb31e5faa91a94ca
Nuno <nuno_c11@hotmail.com>
2021-03-12 12:35:25 +0000

    adjusted validate_r function error checking, still working on validate floor or ceil

--- cub3d/libs/mylib/cub3d/validators.c
+++ cub3d/libs/mylib/cub3d/validators.c
@@ -6,7 +6,7 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 23:22:37 by ngregori          #+#    #+#             */
-/*   Updated: 2021/03/12 12:00:10 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/03/12 12:35:00 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,45 @@ void	validate_r(t_scene *settings, char **strs)
 {
 	t_res *resolution;
 
-	if (!settings->res)
+	if (!strs[1] || !strs[2] || settings->res)
 	{
-		if (ft_str_is_numeric(strs[1]) && ft_str_is_numeric(strs[2]))
-		{
-			resolution = malloc(sizeof(t_res));
-			if (!resolution)
-				return ;
-			resolution->x = ft_atoi(strs[1]);
-			resolution->y = ft_atoi(strs[2]);
-		}
+		settings->valid = false;
+		return ;
 	}
-	if (!resolution || resolution->x <= 0 || resolution->y <= 0)
+	if (ft_str_is_numeric(strs[1]) && ft_str_is_numeric(strs[2]))
+	{
+		resolution = malloc(sizeof(t_res));
+		if (!resolution)
+			return ;
+		resolution->x = ft_atoi(strs[1]);
+		resolution->y = ft_atoi(strs[2]);
+	}
+	if (resolution->x <= 0 || resolution->y <= 0)
 		settings->valid = false;
 	else
 		settings->res = resolution;
 }
 
-void	validate_s(t_scene *settings, char **strs)
+void	validate_floor_ceil(t_scene *settings, t_rgb *floor_or_ceil, char **strs)
 {
-	if (settings->sprite)
+	t_rgb *tmp;
+
+	if (!floor_or_ceil)
 	{
-		settings->valid = false;
-		return ;
+		if (ft_str_is_numeric(strs[1]) && ft_str_is_numeric(strs[2]) && ft_str_is_numeric(strs[3]))
+		{
+			tmp = malloc(sizeof(t_rgb));
+			if (!tmp)
+				return ;
+			tmp->r = (unsigned char)ft_atoi(strs[1]);
+			tmp->g = (unsigned char)ft_atoi(strs[2]);
+			tmp->b = (unsigned char)ft_atoi(strs[3]);
+		}
 	}
-
-}
-
-void	validate_f(t_scene *settings, char **strs)
-{
-	if (settings->floor)
-	{
+	if (!floor_or_ceil || tmp->r < 0 || tmp->g < 0 || tmp->b < 0)
 		settings->valid = false;
-		return ;
-	}
-
+	else
+		floor_or_ceil = tmp;
 }
 
 void	validate_c(t_scene *settings, char **strs)
