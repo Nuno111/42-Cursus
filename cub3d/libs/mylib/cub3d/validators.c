428d8821312d56dca85d624a97f177c55cc22cf6
Nuno <nuno_c11@hotmail.com>
2021-03-12 12:48:31 +0000

    validate floor_ceil and validate r should all be ok now

--- cub3d/libs/mylib/cub3d/validators.c
+++ cub3d/libs/mylib/cub3d/validators.c
@@ -6,7 +6,7 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 23:22:37 by ngregori          #+#    #+#             */
-/*   Updated: 2021/03/12 12:35:00 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/03/12 12:48:14 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,39 +22,43 @@ void	validate_r(t_scene *settings, char **strs)
 		return ;
 	}
 	if (ft_str_is_numeric(strs[1]) && ft_str_is_numeric(strs[2]))
-	{
 		resolution = malloc(sizeof(t_res));
-		if (!resolution)
-			return ;
+	if (resolution)
+	{
 		resolution->x = ft_atoi(strs[1]);
 		resolution->y = ft_atoi(strs[2]);
+		if (resolution->x <= 0 || resolution->y <= 0)
+			settings->valid = false;
+		else
+			settings->res = resolution;
 	}
-	if (resolution->x <= 0 || resolution->y <= 0)
-		settings->valid = false;
 	else
-		settings->res = resolution;
+		settings->valid = false;
 }
 
 void	validate_floor_ceil(t_scene *settings, t_rgb *floor_or_ceil, char **strs)
 {
 	t_rgb *tmp;
 
-	if (!floor_or_ceil)
+	if (!strs[1] || !strs[2] || !strs[3] || floor_or_ceil)
 	{
-		if (ft_str_is_numeric(strs[1]) && ft_str_is_numeric(strs[2]) && ft_str_is_numeric(strs[3]))
-		{
-			tmp = malloc(sizeof(t_rgb));
-			if (!tmp)
-				return ;
-			tmp->r = (unsigned char)ft_atoi(strs[1]);
-			tmp->g = (unsigned char)ft_atoi(strs[2]);
-			tmp->b = (unsigned char)ft_atoi(strs[3]);
-		}
-	}
-	if (!floor_or_ceil || tmp->r < 0 || tmp->g < 0 || tmp->b < 0)
 		settings->valid = false;
+		return ;
+	}
+	if (ft_str_is_numeric(strs[1]) && ft_str_is_numeric(strs[2]) && ft_str_is_numeric(strs[3]))
+		tmp = malloc(sizeof(t_rgb));
+	if (tmp)
+	{
+		tmp->r = (unsigned char)ft_atoi(strs[1]);
+		tmp->g = (unsigned char)ft_atoi(strs[2]);
+		tmp->b = (unsigned char)ft_atoi(strs[3]);
+		if (tmp->r < 0 || tmp->g < 0 || tmp->b < 0)
+			settings->valid = false;
+		else
+			floor_or_ceil = tmp;
+	}
 	else
-		floor_or_ceil = tmp;
+		settings->valid = false;
 }
 
 void	validate_c(t_scene *settings, char **strs)
