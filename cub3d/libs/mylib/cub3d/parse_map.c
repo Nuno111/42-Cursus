d0e30d78119a2e7999b77363d6d04787f2f92d6a
Nuno <nuno_c11@hotmail.com>
2021-03-13 23:33:54 +0000

    parsing map going well, need to create replace_tabs function now

--- cub3d/libs/mylib/cub3d/parse_map.c
+++ cub3d/libs/mylib/cub3d/parse_map.c
@@ -6,28 +6,36 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 23:25:30 by ngregori          #+#    #+#             */
-/*   Updated: 2021/03/13 22:58:39 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/03/13 23:32:45 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static	char	*replace_tabs(t_scene *settings)
+{
+
+}
 
 static	void	linked_to_array(t_scene *settings)
 {
 	int		size;
 	char	**arr;
 	int		i;
-	t_list	tmp_node;
+	t_list	*tmp_node;
 
 	size = ft_lstsize(settings->tmp_map);
 	arr = malloc(sizeof(char *) * size);
 	if (!arr)
 		return 	;
-	tmp_node = *(settings->tmp_map);
-	while (tmp_node.content)
+	while (settings->tmp_map)
 	{
-		arr[i] = ft_strdup(tmp_node.content);
-		tmp_node = *tmp_node.next;
+		tmp_node = settings->tmp_map;
+		settings->tmp_map = settings->tmp_map->next;
+		if (ft_strchr(tmp_node->content, '\t'))
+			arr[i] = replace_tabs(tmp_node->content);
+		else
+			arr[i] = ft_strdup(tmp_node->content);
 		i++;
 	}
 	ft_free_list(&settings->tmp_map);
@@ -37,6 +45,6 @@ void	validate_map(t_scene *settings)
 {
 	if (!settings->valid)
 		return ;
+	replace_tabs(settings);
 	linked_to_array(settings);
-
 }
