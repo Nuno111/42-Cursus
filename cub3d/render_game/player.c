03bf2957234a624d4c315cb117b6b5cbbd4d2d84
Nuno <nuno_c11@hotmail.com>
2021-03-29 15:21:04 +0100

    player refactored

--- cub3d/render_game/player.c
+++ cub3d/render_game/player.c
@@ -6,11 +6,23 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 13:30:45 by ngregori          #+#    #+#             */
-/*   Updated: 2021/03/29 14:48:45 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/03/29 15:20:45 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static	void	get_player_rotation(char c, t_game *game)
+{
+	if (c == 'N')
+		game->player.rotation_angle = M_PI / 2;
+	else if (c == 'E')
+		game->player.rotation_angle = 0;
+	else if (c == 'S')
+		game->player.rotation_angle = 2 * M_PI / 3;
+	else if (c == 'W')
+		game->player.rotation_angle = M_PI;
+}
 
 static	void	get_player_pos(t_game *game)
 {
@@ -25,38 +37,31 @@ static	void	get_player_pos(t_game *game)
 			w++;
 		if (ft_strchr("NESW", game->settings.map[h][w]))
 		{
-			game->player.circle.x = w * game->settings.tile_size.x;
-			game->player.circle.y = h * game->settings.tile_size.y;
+			game->player.circle.x = (w * game->settings.tile_size.x) + (game->settings.tile_size.x / 2.0) ;
+			game->player.circle.y = h * game->settings.tile_size.y + (game->settings.tile_size.x / 2.0);
 			break;
 		}
 		h++;
 	}
+	get_player_rotation(game->settings.map[h][w], game);
 }
 
-void	init_player(t_game *game, t_scene *settings, int x, int y)
+void	init_player(t_game *game)
 {
-	t_player *player;
-
-	player = malloc(sizeof(t_player));
-	if (!player)
-		error_and_exit(settings, "Error\nUnable to allocate memory for player");
-	player->circle.x = x + settings->tile_size.x / 2.0;
-	player->circle.y = y + settings->tile_size.x / 2.0;
-	player->circle.radius = settings->tile_size.x / 4.0;
-	player->circle.color = 0x7F0000;
-	player->circle.ang = 0;
-	player->turn_dir = 0;
-	player->walk_dir = 0;
-	player->rotation_angle = 0;
-	player->move_speed = 1;
-	player->rotation_speed = 2 * (M_PI / 180);
-	player->line.x = player->circle.x;
-	player->line.y = player->circle.y;
-	player->line.color = 0x008000;
-	player->line.direction = player->rotation_angle;
-	player->line.size = player->circle.radius * 2;
-	draw_circle(&game->img, player->circle);
-	draw_inner_circle(&game->img, player->circle);
-	draw_line(&game->img, player->line);
-	game->player = player;
+	get_player_pos(game);
+	game->player.circle.radius = game->settings.tile_size.x / 4.0;
+	game->player.circle.color = 0x7F0000;
+	game->player.circle.ang = 0;
+	game->player.turn_dir = 0;
+	game->player.walk_dir = 0;
+	game->player.move_speed = 1;
+	game->player.rotation_speed = 2 * (M_PI / 180);
+	game->player.line.x = game->player.circle.x;
+	game->player.line.y = game->player.circle.y;
+	game->player.line.color = 0x008000;
+	game->player.line.direction = game->player.rotation_angle;
+	game->player.line.size = game->player.circle.radius * 2;
+	draw_circle(&game->img, game->player.circle);
+	draw_inner_circle(&game->img, game->player.circle);
+	draw_line(&game->img, game->player.line);
 }
