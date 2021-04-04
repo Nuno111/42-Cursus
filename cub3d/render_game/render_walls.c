e4775446b37c7c17d16107ed1236660991176733
Nuno <nuno_c11@hotmail.com>
2021-04-04 22:04:39 +0100

    a

--- cub3d/render_game/render_walls.c
+++ cub3d/render_game/render_walls.c
@@ -6,7 +6,7 @@
 /*   By: ngregori <ngregori@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 20:07:47 by ngregori          #+#    #+#             */
-/*   Updated: 2021/04/04 21:11:47 by ngregori         ###   ########.fr       */
+/*   Updated: 2021/04/04 22:01:51 by ngregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void	render_walls(t_game *game)
 	printf("%s\n", n_wall.relative_path);
 	n_wall.img = mlx_xpm_file_to_image(game->vars.mlx, n_wall.relative_path, &n_wall.width, &n_wall.height);
 	printf(" Width: %d, Height: %d\n",n_wall.width, n_wall.height);
-	mlx_put_image_to_window(game->vars.mlx, game->vars.win, n_wall.img, 600, 600);
+	printf("%d\n", ((int )n_wall.img[1]));
 }
